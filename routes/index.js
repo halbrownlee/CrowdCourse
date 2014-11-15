ffb6c7a2ef1a2570464ae6c18722d3a9d79fa7af
billzhong <billzhong@gmail.com>
2014-11-15 18:13:03 -0330

    remove unfinished middleware

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', ensureAuthenticated,function(req, res) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Crowd Course'});
 });
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/users')
-}
 
 module.exports = router;
