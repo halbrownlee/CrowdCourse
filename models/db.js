1cf0c4b07d8b2e726595d31a77a60fa66343bb14
billzhong <billzhong@gmail.com>
2014-11-15 17:11:10 -0330

    basic login system

--- models/db.js
+++ models/db.js
@@ -1,10 +1,31 @@
 var mongoose = require("mongoose");
+var bcrypt = require('bcrypt');
 
 var UserSchema = new mongoose.Schema({
-  id: Number,
   username: String,
-  passwordHash: String
+  password: String,
+  email: String
 });
+// Bcrypt middleware
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if(!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt) {
+    if(err) return next(err);
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if(err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+// Password verification
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 mongoose.model('User', UserSchema);
 
 var UniversitySchema = new mongoose.Schema({
@@ -72,4 +93,4 @@ var CourseSchema = new mongoose.Schema({
 });
 mongoose.model('Courses', CourseSchema);
 
-mongoose.connect('mongodb://localhost:27017/db')
+mongoose.connect('mongodb://localhost:27017/db');
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,66 @@
 var express = require('express');
 var router = express.Router();
+var users = require('../models/users');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
+var User = users.User;
+
+var user = new User({username: 'admin', email: 'admin@example.com', password: 'admin'});
+user.save(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('user: ' + user.username + " saved.");
+  }
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  User.findOne({ username: username }, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) return done(err);
+      if(isMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Invalid password' });
+      }
+    });
+  });
+}));
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('index', {title: 'Crowd Course'});
+  res.render('users');
 });
 
-router.post('/login', function(req, res) {
-  res.render('index', {title: 'Login successful');
+router.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err) }
+    if (!user) {
+      return res.redirect('/users')
+    }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.redirect('/');
+    });
+  })(req, res, next);
+});
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
